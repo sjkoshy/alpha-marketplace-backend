6fa15e0000f0128ac07e961b777ec7872a3e2ecc
Rebekah <rebekahmenn@gmail.com>
2023-08-10 15:37:42 -0400

    added routes

--- routes/cars.js
+++ routes/cars.js
@@ -5,6 +5,12 @@ import restrict from "../helpers/restrict.js";
 const router = Router();
 
 router.get("/cars", controllers.getCars);
+router.get("/cars/:make", controllers.searchMake);
+router.get("/cars/:model", controllers.searchModel);
+router.get("/cars/:type", controllers.searchType);
+router.get("/cars/:year", controllers.searchYear);
+router.get("/cars/:color", controllers.searchColor);
+router.get("/cars/:price", controllers.searchPrice);
 router.get("/cars/:id", controllers.getCar);
 router.post("/cars", controllers.createCar);
 router.put("/cars/:id", restrict, controllers.updateCar);
