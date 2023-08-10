fd204c258ab22ae3890bec4fb14f5699725ed43e
Sherilyn Koshy <sherilyn.koshy@gmail.com>
2023-08-10 14:32:26 -0400

    modify schema and install bcrypt

--- models/Car.js
+++ models/Car.js
@@ -3,12 +3,12 @@ import mongoose from 'mongoose';
 const CarSchema = new mongoose.Schema({
   make: String,
   model: String,
-  bodyType: String,
+  type: String,
   year: Number,
   color: String,
-  image: String,
   price: Number,
-  user: String
+  userId: String,
+  image: String
 });
 
 export default mongoose.model('Car', CarSchema);
