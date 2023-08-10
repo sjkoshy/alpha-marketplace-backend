e50eb50370e85900ae4ce99f529f639c56033ef0
Rebekah <rebekahmenn@gmail.com>
2023-08-09 21:17:11 -0400

    created 2 basic seed data files: cars and users

---
+++ seed/users.js
@@ -0,0 +1,41 @@
+// import db from "../db/connection.js";
+
+const users = [
+  {
+    _id: "5f3a1291a123b54204c1a123",
+    username: "johnDoe",
+    email: "johndoe@example.com",
+    password: "hashedPassword1",
+    profilePicture: "https://example.com/johndoe.jpg",
+  },
+  {
+    _id: "5f3a1291a123b54204c1a124",
+    username: "janeSmith",
+    email: "janesmith@example.com",
+    password: "hashedPassword2",
+    profilePicture: "https://example.com/janesmith.jpg",
+  },
+  {
+    _id: "5f3a1291a123b54204c1a125",
+    username: "aliceJones",
+    email: "alicejones@example.com",
+    password: "hashedPassword3",
+    profilePicture: "https://example.com/alicejones.jpg",
+  },
+  {
+    _id: "5f3a1291a123b54204c1a126",
+    username: "bobMiller",
+    email: "bobmiller@example.com",
+    password: "hashedPassword4",
+    profilePicture: "https://example.com/bobmiller.jpg",
+  },
+  {
+    _id: "5f3a1291a123b54204c1a127",
+    username: "charlieBrown",
+    email: "charliebrown@example.com",
+    password: "hashedPassword5",
+    profilePicture: "https://example.com/charliebrown.jpg",
+  },
+];
+
+module.exports = users;
