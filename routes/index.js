dd8c8de8271472d3513858276c96b15c71ec60c2
Rebekah <rebekahmenn@gmail.com>
2023-08-10 14:36:36 -0400

    updated index routes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import carsRoutes from "./cars.js";
+import usersRoutes from "./users.js";
 
 const router = Router();
 
@@ -6,6 +8,7 @@ router.get("/", (req, res) => {
   res.send("This is root!");
 });
 
-router.use("/cars", carsRouter);
+router.use("/", carsRoutes);
+router.use("/", usersRoutes);
 
 export default router;
