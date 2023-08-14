b7138902b9a97bc2a756fb3820c06080689d8eb8
Sherilyn Koshy <sherilyn.koshy@gmail.com>
2023-08-14 14:58:06 -0400

    Change route getUserIdByUsername and controller logic

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ import * as controllers from "../controllers/users.js";
 const router = Router();
 
 router.get("/:id", controllers.getUser);
-router.get("/username/:username", controllers.getUserByUsername);
+router.get("/username/:username", controllers.getUserIdByUsername);
 router.post("/signup", controllers.signUp);
 router.post("/login", controllers.signIn);
 router.get("/verify", controllers.verify);
